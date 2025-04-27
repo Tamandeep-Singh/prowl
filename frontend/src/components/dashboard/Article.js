09f5ae4d3e93403b8e970f838ed6d9d16278f513
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2025-04-27 13:20:38 +0100

    Added SQL Injection Article

--- frontend/src/components/dashboard/Article.js
+++ frontend/src/components/dashboard/Article.js
@@ -7,7 +7,7 @@ const Article = ({ article, slug, renderEntirePage = false}) => {
 
     const renderArticle = () => {
         return <Card sx = {{ height: 350, width: 300}} onClick={() => navigate(`/dashboard/education/articles/${slug}`)}>
-            <CardMedia component="img" image={article.image}/>
+            <CardMedia height="180" component="img" image={article.image}/>
             <CardContent>
                 <Typography variant="h6">{article.title}</Typography>
                 <Typography sx={{ fontSize: 16, marginTop: 0.5}} color="text.secondary">{article.date_created}</Typography>
@@ -18,14 +18,15 @@ const Article = ({ article, slug, renderEntirePage = false}) => {
 
     const renderEntireArticle = () => {
        return <div style={{ width: "80vw", display: "flex", justifyContent: "center"}}>
-        <Card sx = {{ marginTop: 3, height: 600, width: 600, paddingLeft: 2, paddingRight: 2}} onClick={() => navigate(`/dashboard/education/articles/${slug}`)}>
-            <CardContent>
-                <Typography variant="h5" sx= {{ marginBottom: 1 }}>{article.title}</Typography>
-                <Typography sx={{ fontSize: 16, marginTop: 0.5}} color="text.secondary">Published on {article.date_created}</Typography>
-                <Divider sx= {{ marginTop: 1, marginBottom: 2}} />
-                <Typography sx={{ fontSize: 16, marginTop: 0.5}} color="text.secondary" dangerouslySetInnerHTML = {{ __html: DOMPurify.sanitize(article.content)}}></Typography>
-                <Divider sx= {{ marginTop: 2, marginBottom: 1}} />
-                <Typography sx={{ fontSize: 12, marginTop: 1.5}} color="text.secondary">Tags: {article.tags.join(", ")}</Typography>
+        <Card sx = {{ marginTop: 3, height: 750, width: 1200}} onClick={() => navigate(`/dashboard/education/articles/${slug}`)}>
+                <CardMedia sx={{objectFit: "cover", width: "100%", height: 180, marginBottom: -1}} component="img" image={article.image}/>
+                <CardContent>
+                    <Typography variant="h5" sx= {{ marginBottom: 1}}>{article.title}</Typography>
+                    <Typography sx={{ fontSize: 16, marginTop: 0.5}} color="text.secondary">Published on {article.date_created}</Typography>
+                    <Divider sx= {{ marginTop: 1, marginBottom: 2}} />
+                    <Typography sx={{ fontSize: 16, marginTop: 0.5}} color="text.secondary" dangerouslySetInnerHTML = {{ __html: DOMPurify.sanitize(article.content)}}></Typography>
+                    <Divider sx= {{ marginTop: 2, marginBottom: 1}} />
+                    <Typography sx={{ fontSize: 12, marginTop: 1.5}} color="text.secondary">Tags: {article.tags.join(", ")}</Typography>
             </CardContent>
         </Card>
        </div>
