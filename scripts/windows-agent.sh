6fe2355b6f27718853fd83bf4ddbf91631d639cf
Tamandeep-Singh <tamandeep.singh.business@gmail.com>
2025-04-30 17:13:04 +0100

    Added Windows Agent Script (still needs finishing for processes)

--- scripts/windows-agent.sh
+++ scripts/windows-agent.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-echo -e "[Prowl-Linux-Agent]: Script started... \n"
+echo -e "[Prowl-Windows-Agent]: Script started... \n"
 
 PROWL_API_ENDPOINT="http://192.168.1.79:4500/api/endpoints/"
-DEVICE_UUID=$(sudo dmidecode -s system-uuid)
+DEVICE_UUID=$(powershell -Command "(Get-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\Cryptography').MachineGuid")
 DEVICE_NAME=$(hostname)
 
 function ingest_request_handler() {
     local POST_BODY="$1"
     response=$(curl -s -X POST -H "Content-Type: application/json" -d "$POST_BODY" "$PROWL_API_ENDPOINT/ingest")
     if [ $? -ne 0 ]; then
-        echo "[Prowl-Linux-Agent]: Unable to connect to the Prowl API, exiting telemetry script!"
+        echo "[Prowl-Windows-Agent]: Unable to connect to the Prowl API, exiting telemetry script!"
         exit 1
     fi
     api_error=$(echo "$response" | jq 'if .result | type == "object" then .result.error // null else null end')
      if [ "$api_error" != "null" ]; then
-        echo "[Prowl-Linux-Agent]: An API Error occurred: $api_error. The script will now exit!"
+        echo "[Prowl-Windows-Agent]: An API Error occurred: $api_error. The script will now exit!"
         exit 1
     else
         echo "$response"
@@ -26,21 +26,21 @@ function link_endpoint_request_handler() {
     local POST_BODY="$1"
     response=$(curl -s -X POST -H "Content-Type: application/json" -d "$POST_BODY" "$PROWL_API_ENDPOINT/link")
     if [ $? -ne 0 ]; then
-        echo "[Prowl-Linux-Agent]: Unable to connect to the Prowl API, exiting telemetry script!"
+        echo "[Prowl-Windows-Agent]: Unable to connect to the Prowl API, exiting telemetry script!"
         exit 1
     fi
     api_error=$(echo "$response" | jq 'if .result | type == "object" then .result.error // null else null end')
      if [ "$api_error" != "null" ]; then
-        echo "[Prowl-Linux-Agent]: An API Error occurred: $api_error. The script will now exit!"
+        echo "[Prowl-Windows-Agent]: An API Error occurred: $api_error. The script will now exit!"
         exit 1
     else
-        echo "[Prowl-Linux-Agent]: Endpoint with ID: $DEVICE_UUID is now successfully linked! Please restart the script to start telemetry collection."
+        echo "[Prowl-Windows-Agent]: Endpoint with ID: $DEVICE_UUID is now successfully linked! Please restart the script to start telemetry collection."
     fi
 }
 
 
 function get_process_data() {
-    process_output=$(ps -axo ppid,pid,user,state,lstart,etime,args)
+    exit 1 # still need to port
     #Fields $5 to $9 = lstart, field $10 = etime, field $11 = command (binary being executed), fields onwards are command arguments 
 
     process_json_array=$(echo "$process_output" | awk 'NR > 1 {
@@ -56,19 +56,17 @@ function get_process_data() {
     {print "{\"ppid\": "$1",\"pid\": "$2",\"user\": \""$3"\",\"state\": \""$4"\",\"start_time\": \""$5,$6,$7,$8,$9"\",\"elapsed_time\": \""$10"\",\"command\": \""args"\"}"  } }' | jq -s '.') 
 
     process_json_body="{\"host_uuid\":\""$DEVICE_UUID"\",\"host_name\":\""$DEVICE_NAME"\",\"agent_pid\":"$$",\"ingest_type\":\"processes\", \"processes\":"$process_json_array"}"
-    ingest_request_handler "$process_json_body"
+    echo "$process_json_body"
 }
 
 function get_filesystem_data() { 
-    tmp_dir="/var/tmp"
-    persistent_dir="/tmp"
+    tmp_dir="$HOME/AppData/Local/Temp"
     desktop_dir="$HOME/Desktop"
     documents_dir="$HOME/Documents"
     downloads_dir="$HOME/Downloads"
-    recycle_bin_dir="$HOME/.local/share/Trash/files"
     
     files_json_array=""
-    files=$(sudo find "$tmp_dir" "$persistent_dir" "$desktop_dir" "$documents_dir" "$downloads_dir" "$recycle_bin_dir" -type f \( -name "*.py" -o -name "*.sh" -o ! -name "*.*" -o -name "*.zip" -o -name "*.js" -o -name "*.rar" -o -name "*.pkg"  -o -name "*.dmg" -o -name "*.tar.gz" \))
+    files=$(find "$tmp_dir" "$desktop_dir" "$documents_dir" "$downloads_dir" -type f \( -name "*.py" -o -name "*.sh" -o ! -name "*.*" -o -name "*.zip" -o -name "*.js" -o -name "*.rar" -o -name "*.pkg"  -o -name "*.dmg" -o -name "*.tar.gz" \))
 
     while read file; do 
         sha256_hash=$(sha256sum "$file" | awk '{print $1}')
@@ -87,21 +85,21 @@ function get_filesystem_data() {
 }
 
 function get_network_data() {
-    network_json_array=$(python3 network.py)
+    network_json_array=$(python network.py)
     network_json_body="{\"host_uuid\":\""$DEVICE_UUID"\",\"host_name\":\""$DEVICE_NAME"\",\"ingest_type\":\"network\", \"network_connections\":"$network_json_array"}"
     ingest_request_handler "$network_json_body"
 }
 
 function link_endpoint() { 
-    device_ip=$(hostname -I | awk '{print $1}')
-    device_os_version=$(hostnamectl | awk -F: '/Operating System/ {print $2}' | xargs)
-    endpoint_telemetry="{\"endpoint\":{\"host_uuid\":\""$DEVICE_UUID"\",\"host_name\":\""$DEVICE_NAME"\",\"host_os\":\"Linux\",\"host_ip\":\""$device_ip"\",\"host_os_version\":\""$device_os_version"\"}}"
+    device_ip=$(ipconfig | grep -m 1 "IPv4 Address" | awk -F: '{print $2}' | xargs)
+    device_os_version="$(systeminfo | grep "^OS Name" | awk -F: '{print $2}' | xargs) ($(systeminfo | grep "^OS Version" | awk -F: '{print $2}' | xargs))"
+    endpoint_telemetry="{\"endpoint\":{\"host_uuid\":\""$DEVICE_UUID"\",\"host_name\":\""$DEVICE_NAME"\",\"host_os\":\"Windows\",\"host_ip\":\""$device_ip"\",\"host_os_version\":\""$device_os_version"\"}}"
     link_endpoint_request_handler "$endpoint_telemetry"
 }
 
 
 function main() {
-    echo -e "[Prowl-Linux-Agent]: Starting Endpoint Telemetry Collection \n"
+    echo -e "[Prowl-Windows-Agent]: Starting Endpoint Telemetry Collection \n"
 
     TUNING_PROCESS_DATA_DELAY=5
     TUNING_FILESYSTEM_DATA_DELAY=60
@@ -139,6 +137,6 @@ case "$1" in
     link_endpoint
     ;;
   *)
-    main
+    get_process_data
     ;;
 esac
